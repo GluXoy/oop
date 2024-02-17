e0e4fc4e0e41415addd217556778f057f6194ab3
GluXoy <mahaonterricon@gmail.com>
2024-02-17 03:28:40 +0300

    разбивка по модулям + тесты

---
+++ 01/Radix/radix_tests/radix_tests.cpp
@@ -0,0 +1,90 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../../../catch2/catch.hpp"
+#include "../Radix/Radix.h"
+
+
+TEST_CASE("CharToInt converts characters to integers correctly", "[CharToInt]") {
+    SECTION("Digits") {
+        REQUIRE(CharToInt('0') == 0);
+        REQUIRE(CharToInt('5') == 5);
+        REQUIRE(CharToInt('9') == 9);
+    }
+
+    SECTION("Uppercase letters") {
+        REQUIRE(CharToInt('A') == 10);
+        REQUIRE(CharToInt('D') == 13);
+        REQUIRE(CharToInt('Z') == 35);
+    }
+
+    SECTION("Lowercase letters") {
+        REQUIRE(CharToInt('a') == 10);
+        REQUIRE(CharToInt('d') == 13);
+        REQUIRE(CharToInt('z') == 35);
+    }
+
+    SECTION("Invalid characters") {
+        REQUIRE_THROWS_AS(CharToInt('$'), std::invalid_argument);
+        REQUIRE_THROWS_AS(CharToInt(':'), std::invalid_argument);
+        REQUIRE_THROWS_AS(CharToInt('Ш'), std::invalid_argument);
+    }
+}
+
+TEST_CASE("IntToString converts integers to strings correctly", "[IntToString]") {
+    SECTION("Decimal to binary") {
+        REQUIRE(IntToString(10, 2) == "1010");
+        REQUIRE(IntToString(15, 2) == "1111");
+        REQUIRE(IntToString(16, 16) == "10");
+    }
+
+    SECTION("Decimal to hexadecimal") {
+        REQUIRE(IntToString(26, 16) == "1A");
+        REQUIRE(IntToString(255, 16) == "FF");
+    }
+
+    SECTION("Negative numbers") {
+        REQUIRE(IntToString(-123, 10) == "-123");
+        REQUIRE(IntToString(-10, 2) == "-1010");
+        REQUIRE(IntToString(-26, 16) == "-1A");
+    }
+
+    SECTION("Zero") {
+        REQUIRE(IntToString(0, 10) == "0");
+        REQUIRE(IntToString(0, 2) == "0");
+        REQUIRE(IntToString(0, 6) == "0");
+        REQUIRE(IntToString(0, 8) == "0");
+        REQUIRE(IntToString(0, 13) == "0");
+        REQUIRE(IntToString(0, 16) == "0");
+        REQUIRE(IntToString(0, 36) == "0");
+    }
+}
+
+TEST_CASE("StringToInt converts strings to integers correctly", "[StringToInt]") {
+    SECTION("Binary") {
+        REQUIRE(StringToInt("1010", 2) == 10);
+        REQUIRE(StringToInt("1111", 2) == 15);
+    }
+
+    SECTION("Decimal") {
+        REQUIRE(StringToInt("123", 10) == 123);
+        REQUIRE(StringToInt("0", 10) == 0);
+    }
+
+    SECTION("Hexadecimal") {
+        REQUIRE(StringToInt("1A", 16) == 26);
+        REQUIRE(StringToInt("FF", 16) == 255);
+    }
+
+    SECTION("Negative numbers") {
+        REQUIRE(StringToInt("-123", 10) == -123);
+        REQUIRE(StringToInt("-1010", 2) == -10);
+        REQUIRE(StringToInt("-FF", 16) == -255);
+    }
+
+    SECTION("Invalid characters") {
+        REQUIRE_THROWS_AS(StringToInt("120", 2), std::invalid_argument);
+        REQUIRE_THROWS_AS(StringToInt("128", 8), std::invalid_argument);
+        REQUIRE_THROWS_AS(StringToInt("FF", 10), std::invalid_argument);
+        REQUIRE_THROWS_AS(StringToInt("12G", 16), std::invalid_argument);
+        REQUIRE_THROWS_AS(StringToInt("ZZ", 34), std::invalid_argument);
+    }
+}
---
+++ 01/invert/invert/invert.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+const int MATRIX_SIZE = 3;
+
+void multiplyMatrices(float mx1[MATRIX_SIZE][MATRIX_SIZE],
+                        float mx2[MATRIX_SIZE][MATRIX_SIZE],
+                        float result[MATRIX_SIZE][MATRIX_SIZE])
+{
+    for (int i = 0; i < MATRIX_SIZE; ++i)
+    {
+        for (int j = 0; j < MATRIX_SIZE; ++j)
+        {
+            result[i][j] = 0;
+            for (int k = 0; k < MATRIX_SIZE; ++k)
+            {
+                result[i][j] += mx1[i][k] * mx2[k][j];
+            }
+        }
+    }
+}
+
+void printMatrix(float mx[MATRIX_SIZE][MATRIX_SIZE])
+{
+    for (int i = 0; i < MATRIX_SIZE; ++i)
+    {
+        for (int j = 0; j < MATRIX_SIZE; ++j)
+        {
+            cout << fixed << setprecision(3) << mx[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Usage: " << argv[0] << " <matrix file1> <matrix file2>" << endl;
+        return 1;
+    }
+
+    ifstream file1(argv[1]);
+    ifstream file2(argv[2]);
+
+    if (!file1 || !file2) {
+        cout << "Error: Unable to open input files." << endl;
+        return 1;
+    }
+
+    float mx1[MATRIX_SIZE][MATRIX_SIZE];
+    float mx2[MATRIX_SIZE][MATRIX_SIZE];
+
+    for (int i = 0; i < MATRIX_SIZE; ++i)
+    {
+        for (int j = 0; j < MATRIX_SIZE; ++j) 
+        {
+            if (!(file1 >> mx1[i][j]) || !(file2 >> mx2[i][j]))
+            {
+                cout << "Error: Invalid input format." << endl;
+                return 1;
+            }
+        }
+    }
+
+    file1.close();
+    file2.close();
+
+    float result[MATRIX_SIZE][MATRIX_SIZE];
+
+    multiplyMatrices(mx1, mx2, result);
+    printMatrix(result);
+
+    return 0;
+}
