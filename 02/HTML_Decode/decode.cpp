4c980b1456ab1b549f4417d90c71288592aa2fb1
GluXoy <124980658+GluXoy@users.noreply.github.com>
2024-03-23 11:27:09 +0300

    Add files via upload

---
+++ 02/HTML_Decode/decode.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include "decode.h"
+
+using namespace std;
+
+vector<Pair> replacements =
+{
+	{"&quot;", "\""},
+	{"&apos;", "'"},
+	{"&lt;", "<"},
+	{"&gt;", ">"},
+	{"&amp;", "&"}
+};
+
+string HtmlDecode(string const& html, const vector<Pair>& replacements)
+{
+	string str = html;
+	const char ENCODE_EL = '&';
+
+	size_t pos = 0;
+	while ((pos = str.find(ENCODE_EL, pos)) != string::npos)
+	{
+		bool isEncoded = false;
+		for (const auto& pair : replacements)
+		{
+			//сделать эффективнее вместо find
+			if (str.find(pair.encode, pos) == pos)
+			{
+				str.replace(pos, pair.encode.size(), pair.decode);
+				pos += pair.decode.size();
+				isEncoded = true;
+				break;
+			}
+		}
+		if (!isEncoded)
+		{
+			++pos;
+		}
+	}
+
+	return str;
+}
