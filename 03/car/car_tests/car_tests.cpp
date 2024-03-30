1518e220598383f61d2f1bcf7d983265d5678299
GluXoy <mahaonterricon@gmail.com>
2024-03-30 09:55:42 +0300

    add simple first test

---
+++ 03/car/car_tests/car_tests.cpp
@@ -0,0 +1,30 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../../../catch2/catch.hpp"
+#include "../car/CarClass.h"
+#include "../car/CarControlClass.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+TEST_CASE("Checking car's commands")
+{
+    Car car;
+    CarControl carControl(car);
+
+    SECTION("Test Info Command")
+    {
+        std::stringstream output;
+        std::streambuf* oldCout = std::cout.rdbuf();
+        std::cout.rdbuf(output.rdbuf());
+        
+        std::stringstream input("Info");
+        std::streambuf* oldCin = std::cin.rdbuf();
+        std::cin.rdbuf(input.rdbuf());
+
+        carControl.HandleCommand();
+
+        std::cin.rdbuf(oldCin);
+        std::cout.rdbuf(oldCout);
+        CHECK(output.str() == "Engine: turned off\nDirection: standing still\nGear: 0\nSpeed: 0\n");
+    }
+}
